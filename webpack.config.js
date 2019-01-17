fe483473937dfa2d0c2519b1fcbab2a964684eb7
bailinhuang <bailinhuangq@hotmail.com>
2019-01-17 16:48:18 -0600

    added hot reload and different configs for prod and dev

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,13 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+
 const config = {
+  mode: "production",
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.join(__dirname, 'dist'),
     filename: './main.js'
-  }, 
+  },
   devServer: {
     contentBase: path.join(__dirname, "dist"),
     compress: true,
@@ -13,22 +15,29 @@ const config = {
     watchContentBase: true,
     progress: true
   },
+  plugins: [new HtmlWebpackPlugin({
+    title: 'React Boilerplate',
+    template: './src/index.html'
+  })],
+  devtool: 'source-map',
+  externals: {
+    react: 'React',
+    'react-dom': 'ReactDOM'
+  },
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
-        use: 'babel-loader',
-        exclude: /node_modules/
-      },{
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        options: {
+          presets: ['@babel/preset-env', '@babel/preset-react'],
+          plugins: [ "@babel/plugin-proposal-class-properties", 'react-hot-loader/babel' ]
+        }
+      }, {
         test: /\.css$/,
         use: [
-          "style-loader",
-          {
-            loader: "css-loader",
-            options: {
-              modules: true
-            }
-          }
+          "style-loader", "css-loader"
         ]
       }
     ]
