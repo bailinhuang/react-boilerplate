df85da8157e098f15bf8aaf3d64866fe614bf556
bailinhuang <bailinhuangq@hotmail.com>
2019-01-18 10:42:00 -0600

    config eslint prettier

--- src/component/app.component.js
+++ src/component/app.component.js
@@ -3,28 +3,27 @@ import s from './app.component.css'
 import { hot } from 'react-hot-loader'
 class App extends Component {
 
-  state = {
-    count: 0
-  }
+  // state = {
+  //   count: 0
+  // }
 
-  addCount = () => {
-    this.setState(state => ({count: state.count + 1}))  
-  }
+  // addCount = () => {
+  //   this.setState(state => ({count: state.count + 1}))  
+  // }
 
-  decrement = () => {
-    this.setState(state => ({count: state.count - 1}))
-  }
+  // decrement = () => {
+  //   this.setState(state => ({count: state.count - 1}))
+  // }
   
-  render() {
-    debugger
+  render() { 
     return ( 
       <div>  
         <h1 >Hello World </h1>
         <div>
-         Count: {this.state.count}
+          {/* Count: {this.state.count} */}
         </div>
         <div>
-          <button onClick={this.addCount}>+</button>
+          {/* <button onClick={this.addCount}>+</button> */}
         </div>
       </div>
     )
